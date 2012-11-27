d21a929153872660e3e8b386906cd9c89e669642
Michael Yahner <mjy8@zips.uakron.edu>
2012-11-27 13:22:30 -0500

    set values for sorted array
    reset sorted array after sorting
    return array values for display

--- Sorting.h
+++ Sorting.h
@@ -5,87 +5,115 @@ class Sorting
 {
 public:
 	Sorting(T* values, int size){
-		//unsorted = values;
+		unsorted = values;
+		sorted = new T[size];
+		for(int i = 0; i < size; ++i){
+			sorted[i] = unsorted[i];
+		}
+		arrSize = size;
 	}
 
 	~Sorting(void){
 	}
 
-	void quickSort(T* arr, int l, int r){
+	void quickSort(T* values, int l, int r){
 		int i=l,j=r,temp;
-		int piv=arr[(l+r)/2];
+		int piv=values[(l+r)/2];
 		
 		while(i<=j){
-		    while(arr[i]<piv){i++;}
-		    while(arr[j]>piv){j--;}
+		    while(values[i]<piv){i++;}
+		    while(values[j]>piv){j--;}
 		    if(i<=j){
-		        temp=arr[i];
-		        arr[i]=arr[j];
-		        arr[j]=temp;
+		        temp=values[i];
+		        values[i]=values[j];
+		        values[j]=temp;
 		        i++;
 		        j--;
 		    }
 		}
 
-		if(l<j){quickSort(arr,l,j);}
-		if(i<r){quickSort(arr,i,r);}
+		if(l<j){quickSort(values,l,j);}
+		if(i<r){quickSort(values,i,r);}
 	}
 
-	void insertionSort(T* arr, int size){
+	void insertionSort(int size){
 		int j, temp;
 		for(int i = 1; i < size; ++i){
 			j = i;
-			while(j > 0 && arr[j - 1] > arr[j]){
-				temp = arr[j];
-				arr[j] = arr[j - 1];
-				arr[j - 1] = temp;
+			while(j > 0 && sorted[j - 1] > sorted[j]){
+				temp = sorted[j];
+				sorted[j] = sorted[j - 1];
+				sorted[j - 1] = temp;
 				j--;
 			}
 		}
 	}
 
-	void selectionSort(T* arr, int size){
+	void selectionSort(int size){
 		int index, temp;
 		for(int i = 0; i < size; ++i){
 			index = i;
 			for(int j = i + 1; j < size; ++j){
-				if(arr[j] < arr[index]){
+				if(sorted[j] < sorted[index]){
 					index = j;
 				}
 			}
 			if(index != i){
-				temp = arr[i];
-				arr[i] = arr[index];
-				arr[index] = temp;
+				temp = sorted[i];
+				sorted[i] = sorted[index];
+				sorted[index] = temp;
 			}
 		}
 	}
 
-	void iterMerge(T* arr, int size){
+	void iterMerge(int size){
 		int inc,left,leftMax,right,rightMax,cur;
-		int *temp = new int[size];
-		inc=1;
-		while(inc<size){
-		    left=0;
-		    right=inc;
-		    leftMax=right-1;
-		    rightMax=(leftMax+inc<size)?leftMax+inc:size-1;
-		    cur=0;
-		    while(cur<size){
-		        while(left<=leftMax && right<=rightMax){
-		            if(arr[right]<arr[left]){temp[cur]=arr[right++];}
-		            else{temp[cur]=arr[left++];}
+		int *temp = new T[size];
+		inc = 1;
+		while(inc < size){
+		    left = 0;
+		    right = inc;
+		    leftMax = right - 1;
+		    rightMax = (leftMax + inc < size) ? leftMax + inc : size - 1;
+		    cur = 0;
+		    while(cur < size){
+		        while(left <= leftMax && right <= rightMax){
+		            if(sorted[right] < sorted[left]){
+						temp[cur]=sorted[right++];
+					}
+		            else{
+						temp[cur]=sorted[left++];
+					}
 		            cur++;
 		        }
-		        while(right<=rightMax){temp[cur++]=arr[right++];}
-		        while(left<=leftMax){temp[cur++]=arr[left++];}
+		        while(right<=rightMax){
+					temp[cur++]=sorted[right++];
+				}
+		        while(left<=leftMax){
+					temp[cur++]=sorted[left++];
+				}
 		        left=right;
 		        right+=inc;
 		        leftMax=right-1;
 		        rightMax=(leftMax+inc<size)?leftMax+inc:size-1;
 		    }
 		    inc*=2;
-		    for(int i=0;i<size;i++){arr[i]=temp[i];}
+		    for(int i=0;i<size;i++){
+				sorted[i]=temp[i];
+			}
+		}
+		delete[] temp;
+	}
+
+	T* arrayReturn(){
+		return sorted;
+	}
+
+	void resetArrays()
+	{
+		cout<<arrSize << endl;
+		for(int i = 0; i < arrSize; ++i){
+			sorted[i] = unsorted[i];
 		}
 	}
 
@@ -93,5 +121,6 @@ public:
 private:
 	T* unsorted;
 	T* sorted;
-	void resetArrays();
+
+	int arrSize;
 };
