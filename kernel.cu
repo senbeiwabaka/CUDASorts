e020588dd16a4d355081791630871d35d376e98b
Michael Yahner <mjy8@zips.uakron.edu>
2012-11-30 09:42:43 -0500

    ...
    Was going to make them not prototyped but it bugged out.

--- kernel.cu
+++ kernel.cu
@@ -1,4 +1,4 @@
-__device__ void GPUquickSort(int arr[],int l,int r);
-__device__ void GPUmerge(int arr[],int tempArr[],int x,int mid,int y);
-__device__ void GPUinsertionSort(int arr[],int arrLength);
-__device__ void GPUselectionSort(int arr[],int arrSize);
+__device__ void GPUBubbleSort(int arr[],int l,int r);
+__device__ void GPUIteraMergeSort(int arr[],int tempArr[],int x,int mid,int y);
+__device__ void GPUInsertionSort(int arr[],int arrLength);
+__device__ void GPUSelectionSort(int arr[],int arrSize);
