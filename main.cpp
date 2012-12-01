eeba516b3ac30a9ca8d1548344cfcc0cf8823160
Michael Yahner <mjy8@zips.uakron.edu>
2012-12-01 14:10:09 -0500

    added includes
    windows.h, string, and fstream. since it is using windows.h now - it can
    only run on windows - for now
    removed const size and set a physical 10 in all places - for now.
    now you no longer need to send it an argument. please read me for
    understanding here

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
+#include <Windows.h>
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
 #include <cuda.h>
+#include <string>
+#include <fstream>
 #include "Sorting.h"
 #include "Test.h"
 
 using namespace std;
 
 extern "C" void call(const char* name);
-
-const int SIZE=10;
 
 template <class T>
 const void choice(const char selection, int size, Sorting<T>* s){
@@ -80,15 +81,43 @@ const void choice(const char selection, int size, Sorting<T>* s){
 }
 
 
-int main(int argc, char **argv){
-    int intArray[SIZE]={4,10,3,1,5,9,7,8,6,2};
-    float floatArray[SIZE]={9,7,2,6,3,10,4,1,5,8};
-    char charArray[SIZE]={2,1,9,3,8,10,4,7,6,5};
+int main(int argc, char *argv[]){
+    int intArray[10]={4,10,3,1,5,9,7,8,6,2};
+    float floatArray[10]={9,7,2,6,3,10,4,1,5,8};
+    char charArray[10]={2,1,9,3,8,10,4,7,6,5};
 
 	Sorting<int>* s = new Sorting<int>(intArray, 10);
 
-	cout << argc << endl;
-	cout << "usuage " << argv[1] << endl;
+	if(argc == 2)
+	{
+		string arg = argv[1];
+		if(arg == "-help" || arg == "-h")
+		{
+			cout << "To use this program you can either run it with a txt file for loading in values for the array " <<
+				"or you can run it without a text file and it will randomly fill the array with ints" << endl;
+
+			Sleep(2000);
+
+			exit(0);
+		}
+		else
+		{
+			cout << argv[1] << endl;
+			ifstream myfile (argv[1]);
+			if(myfile.is_open())
+			{
+				string line;
+
+				while(myfile.good())
+				{
+					getline(myfile, line);
+					cout << line << endl;
+				}
+			}
+
+			myfile.close();
+		}
+	}
 
 	cout << endl << endl;
 
