43e12c7ce356af83b69ffbfe4c967e45358d3e0a
Michael Yahner <mjy8@zips.uakron.edu>
2012-12-04 13:45:28 -0500

    rebuilt from the ground up.
    did some templating
    needs lots of work still

--- kernel.cu
+++ kernel.cu
@@ -1,4 +1,48 @@
-__device__ void GPUBubbleSort(int arr[],int l,int r);
-__device__ void GPUIteraMergeSort(int arr[],int tempArr[],int x,int mid,int y);
-__device__ void GPUInsertionSort(int arr[],int arrLength);
-__device__ void GPUSelectionSort(int arr[],int arrSize);
+#ifndef _KERNEL_H_
+#define _KERNEL_H_
+
+#include <iomanip>
+#include "Test.h"
+
+template <class T>
+__global__ void BubbleKernel(int size, T* arr){
+	bool swap = true;
+	int x = 0; int temp;
+	while(swap){
+		swap = false;
+		x++;
+		for( int i = 0; i < size; i++){
+			if(arr[i] > arr[i + 1]){
+				temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				swap = true;
+			}
+		}
+	}
+}
+
+__global__ void InsertionKernel(){
+}
+
+template <class T>
+void call(const char* name, T* arr, int size){
+	char *n = new char[strlen(name)+1];
+	strcpy(n, name);
+	//printf("name %s", n);
+
+	if(name == "bubble"){
+		BubbleKernel<T><<<1, 1>>>(size, arr);
+	}
+}
+
+template void
+call<int>(const char* name, int *arr, int size);
+
+template void
+	call<float>(const char* name, float *arr, int size);
+
+template void
+	call<char>(const char* name, char *arr, int size);
+
+#endif
